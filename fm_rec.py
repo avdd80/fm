1733b975db9a4a326b0c0d4f199f21076cb2fbe6
Abhijeet <avdd80@gmail.com>
2020-06-10 20:06:27 -0700

    bf1

--- fm_rec.py
+++ fm_rec.py
@@ -9,25 +9,22 @@ from time import sleep
 # COMMON ###################################################
 TUNER_PATH = '/home/pi/Music/radio_tea5767/radio_tea5767'
 ROOT_PATH = '/home/pi/Music/fm_db/'
-
+DROPBOX_DOWNLOAD_CMD = 'sudo /home/pi/Downloads/Dropbox-Uploader/dropbox_uploader.sh download '
+DROPBOX_DELETE_CMD = '/home/pi/Downloads/Dropbox-Uploader/dropbox_uploader.sh delete /'
+DROPBOX_LIST_CMD = '/home/pi/Downloads/Dropbox-Uploader/dropbox_uploader.sh list /'
 
 
 # SAN DIEGO ################################################
 STEREO_AUDIO_INJECTOR_REC_CMD = 'sudo arecord -c 2 -f S16_LE -V stereo -r 48000 -d '
 DROPBOX_DOWNLOAD_SCRIPT = 'sudo -S /home/pi/fm/download_schedule.sh'
 SCHED_PATH_F = '/home/pi/Music/schedule.txt'
-DROPBOX_DOWNLOAD_CMD = 'sudo /home/pi/Downloads/Dropbox-Uploader/dropbox_uploader.sh download '
-DROPBOX_DELETE_CMD = '/home/pi/Downloads/Dropbox-Uploader/dropbox_uploader.sh delete /'
-DROPBOX_LIST_CMD = '/home/pi/Downloads/Dropbox-Uploader/dropbox_uploader.sh list /'
+
 FM_stations = {88.3: 'San_Diegos_Jazz', 89.5: 'NPR', 91.1: '91X_XETRA_FM', 93.3: 'Channel93_3', 94.1: 'Star94_1', 94.9: 'San_Diegos_Alternative', 95.7: 'KISSFM', 96.5: 'KYXY', 98.1: 'Sunny_98_1', 101.5: '101KGB_Classic_Rock', 102.9: 'Amor', 105.3: 'ROCK1053', 106.5: 'Que_Buena'}
 
 # BANGALORE ################################################
 #MONO_USB_AUDIO_REC_CMD = 'sudo arecord --device=hw:1,0 -c1 -f S16_LE -V mono -r 44100 -d '
 #DROPBOX_DOWNLOAD_SCRIPT = 'sudo -S /home/pi/fm/download_blr_schedule.sh'
 #SCHED_PATH_F = '/home/pi/Music/schedule_blr.txt'
-#DROPBOX_DOWNLOAD_CMD = 'sudo /home/pi/Downloads/Dropbox-Uploader/dropbox_blr_uploader.sh download '
-#DROPBOX_DELETE_CMD = '/home/pi/Downloads/Dropbox-Uploader/dropbox_blr_uploader.sh delete /'
-#DROPBOX_LIST_CMD = '/home/pi/Downloads/Dropbox-Uploader/dropbox_blr_uploader.sh list /'
 #FM_stations = {91.1: 'Radio City', 98.3: 'Radio Mirchi', 94.3: 'Radio One', 93.5: 'Red FM', 91.9: 'Radio Indigo', 92.7: 'Big FM', 104.0: 'Fever FM', 100.1: 'Amrutavarshini', 90.4: 'Radio Active(Jain)', 102.9: 'Vividh Bharati'}
 
 
